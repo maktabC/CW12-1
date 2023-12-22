02db93baf549739a4ee16ab6653d0ee5b6cb0c82
Navid Jafarzadeh <navid.jfrzadeh@gmail.com>
2023-12-22 16:24:33 +0330

    second commit

---
+++ CW12-1/Controllers/AddressController.cs
@@ -0,0 +1,39 @@
+﻿using CW12_1.Models;
+using CW12_1.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW12_1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AddressController : ControllerBase
+{
+    Serilization serialize = new Serilization();
+    DataAccsess dataAccess = new DataAccsess("C:\\Users\\Navid\\Documents\\My Files\\MaktabC#\\CW12-1\\new\\CW12-1_2\\address.txt");
+
+    [HttpPost("SaveAddress")]
+    public IActionResult SaveAddress([FromForm] string address)
+    {
+        var addressJson = serialize.SerilizeToJson(new Address { AddressName = address });
+        dataAccess.SaveToFile(addressJson);
+        return Ok();
+    }
+
+    [HttpGet("GetAddress")]
+    public IActionResult GetAddress( [FromQuery]string id)
+    {
+        var addressArr = dataAccess.ReadFile();
+
+        foreach (var address in addressArr)
+        {
+            var currentAdderss = serialize.DeserilizeJson(address);
+            if (currentAdderss.AddressID == id)
+            {
+                return Ok(currentAdderss);
+            }
+        }
+        
+        return NotFound();
+    }
+}
---
+++ CW12-1/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace CW12_1.Models;
+
+public class Address
+{
+    public Address()
+    {
+        AddressID = Guid.NewGuid().ToString();
+    }
+
+    public string AddressName { get; set; }
+    public string AddressID { get; set; }
+
+    
+}
